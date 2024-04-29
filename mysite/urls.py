b6bee90c54429aacf8bb998f4b8ce9888642fc53
Endami <157072524+Endami@users.noreply.github.com>
2024-04-28 21:05:39 -0400

    update

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,3 +19,10 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+from django.urls import path
+from blog import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
